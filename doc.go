dd244a69ae9d2678ace39eced3e3013b5c33433c
Aakriti Shroff <aakriti@mit.edu>
2015-06-27 21:53:40 -0400

    initial ws server with chat app

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+
+Groot is a Websocket server in Golang implementing the Social Linked Data infrastructure.
+Author: Aakriti Shroff <aakriti@mit.edu>
+
+*/
+
+package main
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
+	"text/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, Loving %s!", r.URL.Path[1:])
+var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("home.html"))
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	homeTempl.Execute(w, r.Host)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	flag.Parse()
+	go h.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", serveWs)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
